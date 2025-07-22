708d21f0d8640744646c3d3c6c0b68c337ad2582
Biokes <106444765+Biokes@users.noreply.github.com>
2025-07-22 20:30:48 +0100

    [main] (UPDATE): added network for localhost

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,26 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { SEPOLIA_PRIVATE_KEY, SEPOLIA_URL } = process.env;
-
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 const config: HardhatUserConfig = {
   solidity: "0.8.30",
+   etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+    },
   networks: {
-    sepolia: {
-      url: SEPOLIA_URL,
-      accounts: [SEPOLIA_PRIVATE_KEY??""]
-    }
-  }
+    localhost: {
+        url: "http://127.0.0.1:8545",
+      },
+   }
+  // networks: {
+  //   sepolia: {
+  //     url: SEPOLIA_URL,
+  //     accounts: [SEPOLIA_PRIVATE_KEY??""]
+  //   }
+  // }
 };
 
 export default config;
