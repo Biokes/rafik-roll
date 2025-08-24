9eb7f2798430d499dc4ba96126dcd9c0ce826a55
Biokes <106444765+Biokes@users.noreply.github.com>
2025-08-24 22:57:19 +0100

    add test

--- test/rafikGame.ts
+++ test/rafikGame.ts
@@ -3,22 +3,20 @@ import { expect } from "chai";
 
 
 describe("rafik game test suite", function () {
-    var RafikGame, game, RafikToken, token, Generator, generator;
+    var RafikGame, game, RafikToken, token, Generator, generator, ;
     var p1, p2, p3, p4, p5, p6;
     const BASE_FEE = ethers.parseEther("1");
     const deployAll = async function () { 
-        RafikToken = await ethers.getContractFactory("Rafikk");
-        token = await RafikToken.deploy()
-        Generator = await ethers.getContractFactory("RafikGenerator");
+        Generator = await ethers.getContractFactory("TestGenerator");
         generator = await Generator.deploy();
+        RafikToken = await ethers.getContractFactory("Rafikk",);
+        token = await RafikToken.deploy()
 
     }
     describe("player game entrance", function () {
-        
         it("tests 4 players can join a game", async function () { 
             
         })
      })
-
 
 })
