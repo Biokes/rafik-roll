00ee9bd550e801085ebf25740da6d26686c3a4c3
Biokes <106444765+Biokes@users.noreply.github.com>
2025-08-24 21:12:17 +0100

    added test genrator

--- contracts/IRafikGenerator.sol
+++ contracts/IRafikGenerator.sol
@@ -2,7 +2,5 @@
 pragma solidity ^0.8.30;
 
 interface IRafikGenerator {
-    function requestRandomWords(bool enableNativePayment) external returns (uint256 requestId);
-    function getRequestStatus(uint256 _requestId) external view returns (bool fulfilled, uint256[] memory randomWords);
     function getRandomNumber() external returns(uint numberGenerated);
 }
---
+++ contracts/TestGenerator.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier : MIT
+pragma solidity ^0.8.30;
+
+import {IRafikGenerator} from "./RafikGenerator.sol";
+
+contract TestGenerator is IRafikGenerator {
+    uint256 private nonce;
+
+    function getRandomNumber() external returns (uint) {
+        uint256 random = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp,
+                    block.prevrandao,
+                    msg.sender,
+                    nonce
+                )
+            )
+        );
+        nonce++;
+
+        return random % 6;
+    }
+}
