6a7447b8146dd783e95d22669ea8c1459e57066c
Biokes <106444765+Biokes@users.noreply.github.com>
2025-08-27 01:16:51 +0100

    update admin withdrwal and token withdrawal

--- contracts/RafikGame.sol
+++ contracts/RafikGame.sol
@@ -10,40 +10,44 @@ contract RafikGame {
 
     event RandomRequested(address indexed player, uint256 requestId);
     event RandomResolved(uint256 indexed timeStamp,uint randomValues);
+    address private admin ;
 
     constructor(address generatorAddress, address gameTokenAddress) {
         generator = IRafikGenerator(generatorAddress);
         gameToken = IERC20(gameTokenAddress);
+        admin= msg.sender;
     }
 
     struct Game{
         uint gameId;
         bool isActive;
-        address[] players;
+        Player[] players;
         uint roll;
         uint price;
     }
-
+    struct Player{
+        uint roll;
+        address palyerAddress;
+    }
     mapping (uint => Game) private allGames;
-    // mapping (address=> bool) playersActivity;
 
     uint private totalGameCounter = 1000;
     uint constant private BASE_FEE = 1000000000000000000; 
 
-    function joinGame(uint gameId)external {
+    function joinGame(uint gameId,uint roll)external {
         Game storage game = allGames[gameId];
         require(game.players.length<4,"GAME PLAYERS COMPLETE");
         require(game.isActive,"Invalid Game Id Provided");
         require(!isInGame(msg.sender, game),"ALREADY JOINED GAME");
         require(gameToken.balanceOf(msg.sender)> game.price, "INSUFFICIENT BALANCE");
         gameToken.transferFrom(msg.sender, address(this),game.price);
-        game.players.push(msg.sender);
+        game.players.push(Player(roll, msg.sender));
     }
 
     function isInGame(address playerAddress, Game memory game) private pure returns (bool){
         uint index;
         for(;index<game.players.length;){
-            if(game.players[index]== playerAddress) return true;
+            if(game.players[index].playerAddress == playerAddress) return true;
             index++;
         }
         return false;
@@ -56,7 +60,7 @@ contract RafikGame {
         game.roll = roll;
     }
     
-    function createGameWithPrice(uint price)external returns(uint){
+    function createGameWithPrice(uint price,uint roll)external returns(uint){
         require(gameToken.balanceOf(msg.sender) >= price ,"INSUFFICIENT BALANCE");
         require(gameToken.balanceOf(msg.sender)>= BASE_FEE,"PRICE IS LOWER THAN BASE FEE");
         totalGameCounter+=1;
@@ -64,20 +68,21 @@ contract RafikGame {
         Game storage game = allGames[totalGameCounter];
         game.gameId = totalGameCounter;
         game.isActive= true;
-        game.players.push(msg.sender);
+        game.players.push(Player(msg.sender,roll));
         game.price = price;
         return game.gameId;
     }
 
-    function createNewGame()external returns(uint){
+    function createNewGame(uint roll)external returns(uint){
         require(gameToken.balanceOf(msg.sender) >= BASE_FEE,"INSUFFICIENT BALANCE");
         totalGameCounter+=1;
         gameToken.transferFrom(msg.sender, address(this),BASE_FEE);
-        Game storage game = allGames[totalGameCounter];
+        Game memory game;
         game.gameId = totalGameCounter;
         game.isActive= true;
-        game.players.push(msg.sender);
+        game.players.push(Player(msg.sender,roll));
         game.price = BASE_FEE;
+        allGames[totalGameCounter] = game;
         return game.gameId;
     }
 
@@ -85,7 +90,17 @@ contract RafikGame {
         Game storage game = allGames[gameId];
         require(game.isActive && game.players.length>3,"NO WINNER YET");
         rollDice(game.gameId);
-        
         return game.roll;
     }
+
+    recieve()external{}
+
+    fallback()external{}
+
+    function withdraw()external payable{
+        require(admin== msg.sender,"UNAUTHORISED");
+        token.transfer(token.balanceOf(address(this), msg.sender));
+        payable(msg.sender).call{value: address(this).balance}("");
+    }
+
 }
